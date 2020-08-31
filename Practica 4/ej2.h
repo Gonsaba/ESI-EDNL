01bd10e9ada78f140062c46e5b17df59512f6475
Jesús Márquez <jesusmarquezdelgado@gmail.com>
2020-08-31 22:01:39 +0200

    p4

--- Practica 4/ej2.h
+++ Practica 4/ej2.h
@@ -1,6 +1,43 @@
 #ifndef EJ2_H_
 #define EJ2_H_
 
+#include <vector>
+#include "abb.h"
 
+
+template<typename T>
+Abb<T> equilibrar(Abb<T> &A){
+    std::vector<T> vec;
+    vecInorden(A,vec);
+    for(int i = 0; i < vec.size(); ++i)
+        std::cout<<vec[i]<<" ";
+    std::cout<<std::endl;
+
+    Abb<T> B;
+    generarArbol(B,vec,0,vec.size());
+    //imprimirAbb(B);
+    return B;
+}
+
+template<typename T>
+void vecInorden(const Abb<T> &A,std::vector<T> &vec){
+    if(!A.izqdo().vacio())
+        vecInorden(A.izqdo(),vec);
+
+    vec.push_back(A.elemento());
+
+    if(!A.drcho().vacio())
+        vecInorden(A.drcho(),vec);
+}
+
+template<typename T>
+void generarArbol(Abb<T> &A,std::vector<T> vec,int inicio,int fin){
+    int media = ((fin - inicio)/2) + inicio;
+    if(fin - inicio > 0){
+        A.insertar(vec[media]);
+        generarArbol(A,vec,inicio,media-1);
+        generarArbol(A,vec,media+1,fin);
+    }
+}
 
 #endif
--- Practica 4/main.cpp
+++ Practica 4/main.cpp
@@ -2,11 +2,12 @@
 #include "abb.h"
 #include "funciones.h"
 #include "ej1.h"
+#include "ej2.h"
 
 typedef int T;
 
-int main(){
-    Abb<T> A;
+void ej1(){
+	Abb<T> A;
 	T vec[] = {7,5,12,9,17,8,11,10};
 	unsigned tam = sizeof(vec)/sizeof(T);
 	for(unsigned i = 0; i < tam; ++i)
@@ -16,4 +17,17 @@ int main(){
 	imprimirAbb(A);
 	poda(A,9);
     imprimirAbb(A);
+}
+
+int main(){
+    Abb<T> A,B;
+	T vec[] = {7,5,12,9,17,8,11,10};
+	unsigned tam = sizeof(vec)/sizeof(T);
+	for(unsigned i = 0; i < tam; ++i)
+		A.insertar(vec[i]);
+	T a;
+	
+	imprimirAbb(A);
+	B = equilibrar(A);
+    imprimirAbb(B);
 }
